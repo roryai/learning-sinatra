f43671039d86f8b616aa8a88e9e1dcd2fa744cf4
Rory Aitchison <rory.aitchison@gmail.com>
2016-10-11 11:30:40 +0100

    adds completed challenge 12- introducing params

--- app.rb
+++ app.rb
@@ -5,7 +5,14 @@ get '/' do
 "hello world!"
 end
 
-get '/cat' do
-  @name_sampler = ["Amigo", "Rory", "Jini"].sample
+get '/random-cat' do
+  @name = ["Amigo", "Rory", "Jini"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  # ?name=James
   erb(:index)
 end
